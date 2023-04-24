2def464d830f44361bce9a6a84fed21f88af1920
rudranshbansal <bansalrudransh24@gmail.com>
2023-04-24 10:38:17 +0530

    added new streamlit file

--- hehe.py
+++ hehe.py
@@ -1,10 +1,33 @@
 import streamlit as st
 import requests
 import json
+from PIL import Image
 import streamlit.components.v1 as components
 
-st.title("Weather App") # Displays title
-st.subheader("Exploring RapidAPI's weather Endpoints") # Displays subheader
+st.set_page_config(page_title="Weather Wiz", page_icon=":partly_sunny:", layout="wide")
+
+def add_bg_from_filesystem():
+    video_path = "./animations/sun_rain.mp4"
+    video_file = open(video_path, 'rb')
+    video_bytes = video_file.read()
+    #st.set_page_config(page_title="Weather Wiz", page_icon=":partly_sunny:", layout="wide")
+    st.markdown(
+        f"""
+        <style>
+        .reportview-container {{
+            background: url(data:image/mp4;base64,{video_bytes.decode('latin-1')});
+            background-size: cover;
+            background-repeat: no-repeat;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+add_bg_from_filesystem()
+
+st.title("Weather Wiz")
+st.subheader("Weather around the world at your doorstep!!")
 
 url = "https://weatherapi-com.p.rapidapi.com/current.json"
 
@@ -13,7 +36,7 @@ headers = {
     'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
 }
 
-location = st.text_input("Enter the location", "Chennai")
+location = st.text_input("Enter the location", "Patiala")
 querystring = {"q":{location}}
 
 response = requests.request("GET", url, headers=headers, params=querystring)
@@ -34,31 +57,23 @@ with col1:
     st.write(f'Feels like: {data["current"]["feelslike_c"]} ℃')
 
 with col2: 
-    st.write(f'Temp in Celcius: {data["current"]["temp_c"]}')
+    st.write(f'Temp in Celsius: {data["current"]["temp_c"]}')
     st.write(f'Temp in Farenheit: {data["current"]["temp_f"]}')
     st.write(f'Condition: {data["current"]["condition"]["text"]}')
     st.image(f'http:{data["current"]["condition"]["icon"]}')
     st.metric(label = "Humidity", value = f'{data["current"]["humidity"]}')
     
-st.info('⛅ Current weather or realtime weather API method allows a user to get up-to-date current weather information in json and xml. The data is returned as a Current Object.')
+st.info('⛅ Weather_Wiz is our Computer Graphics Project. It is aimed at providing visualizations of the real-time weather conditions using opengl concepts taught to us in this subject.')
 
+
+video_file = open('./animations/sun_rain.mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+image = Image.open('thapar_bg_logo.png')
+st.image(image)
 components.html(
     """
-    <a href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0" target="_blank"></a>
+    <a href="https://rudranshbansal.github.io/" title="Our previous project">For More Info Click Here</a>
     """
 )
-def add_bg_from_url():
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
-             background-attachment: fixed;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
-
-add_bg_from_url() 
